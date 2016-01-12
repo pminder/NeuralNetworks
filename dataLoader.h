e871bf5c12bbe2500e151d51cda7051ac2ca1cdc
pminder <paul.minder.sanchez@gmail.com>
2016-01-12 19:29:46 +0100

    Début d'implémentation de DataLoader

--- dataLoader.h
+++ dataLoader.h
@@ -2,12 +2,21 @@
 #define DATALOADER_H
 
 #include <string>
+#include <vector>
+#include <valarray>
+
+
+//Quelques constantes : les noms de fichiers
+#define xtrainFile "data/xtrain.mnist"
+#define ytrainFile "data/ytrain.mnist"
+#define xtestFile "data/xtest.mnist"
+#define ytestFile "data/ytest.mnist"
 
 class DataLoader
 {
 public:
 	//Constructeur
-	DataLoader(std::string filename);
+	DataLoader();
 	//Destructeur
 	~DataLoader();
 
@@ -16,6 +25,23 @@ public:
 
 	
 private:
+	//Réference vers la base d'apprentissage (size 50 000)
+	std::vector< std::valarray<float> > * xtrain;
+	//Référence vers les labels d'apprentissage
+	std::vector<char> * ytrain;
+
+	//Réference vers la base de test (size 10 000)
+	std::vector< std::valarray<float> > * xtest;
+	//Référence vers les labels de test
+	std::vector<char> *  ytest;
+
+	//Réference vers la base "validation" size (10 000)
+	std::vector< std::valarray<float> > * xvalid;
+	//Référence vers les labels "validation"
+	std::vector<char> * yvalid;
+
+	std::vector<char> * loadLabels(std::string filename);
+	std::vector< std::valarray<float> > * loadImages(std::string filename);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout << "Hello World!" << endl;
+	DataLoader dataLoader;
+	dataLoader.load();
 	return 0;
 }
