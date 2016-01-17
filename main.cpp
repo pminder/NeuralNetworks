7729e630c83d9b3defda44c1b8824a7cf12f479c
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2016-01-18 00:07:44 +0100

    Update main.cpp
    Update de la main permettant de tester au visu les données pour voir si tout va bien. DataMnist.h, DataMnist.cpp et main.cpp compilent et marchent sur mon ordinateur. Posez moi vos questions si vous avez le moindre problème ;).
    
    C'est pas forcément optimisé niveau allocation de mémoire, mais au moins maintenant, on a la base de donnée et on peut commencer à travailler sur le reste.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <valarray>
 
-#include "dataLoader.h"
-#include "network.h"
+#include "DataMnist.h"
 
-using namespace std;
+using namespace std ;
 
 int main(int argc, char const *argv[])
 {
-	DataLoader dataLoader;
-	dataLoader.load();
-	return 0;
+	DataMnist database;
+
+	//Test of data.loadLabels
+	//We only load the test labels (10 000) 
+	//to save time (this is just a test)
+	cout << "Test of labels" << endl;
+	database.loadLabels(1);
+	//We display the first 10 to see if the data has the right form
+	for (int j = 0; j<10; ++j)
+	{ 
+		cout << database.testLabels(j) << endl;
+	}
+
+
+	//Test of data.loadImages
+	cout << "Test of images" << endl;
+	database.loadImages(1);
+	for (int j = 0; j < 784; j++)
+	{
+		cout << database.testImagesPixels(0,j) << endl;
+	}
+	cin.get();
 }
