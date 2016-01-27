83231de8c7bf254dbb7d5edded04b3f7ad123f2e
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2016-01-27 21:59:08 +0100

    Simplifications et adaptation pour Eigen
    Suppression de pas mal de choses inutiles.
    Rajout de "_" devant les attributs de classe pour respecter les conventions.
    Optimisation du nombre d'opérations lors du chargement de la base de donnée.
    Adaptation pour l'utilisation de Eigen.

--- DataMnist.h
+++ DataMnist.h
@@ -5,8 +5,14 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <Eigen/Dense>
 
-
+//We treat those as constants as this class is only designed to treat the MNIST data base
+//So we do not lose modularity doing this
+const int image_size = 28;
+const int flat_image_size = 28*28 ;
+const int test_size = 10000 ;
+const int train_size = 60000 ;
 
 class DataMnist
 {
@@ -18,59 +24,48 @@ public:
 	DataMnist(std::string data_path);
 
 	~DataMnist(void);
+	
+	//Gives the right dimension to the elements of the database
+	//We do not use static vectors though because the database images will be the output of access methods
+	int initSizes();
+
+	int initFileNames();
 
 	int loadImages(int) ;
 
 	int loadLabels(int) ;
 
 	//Accesors
-	std::vector < std::vector <double> > getTestImages();
+	int trainLabel (int);
 
-	std::vector < std::vector <double> > getTrainImages();
-	
-	std::vector <int> getTrainLabels();
+	int testLabel (int);
 
-	std::vector <int> getTestLabels();
+	Eigen::VectorXd trainImage (int);
 
-	int trainLabels (int);
-
-	int testLabels (int);
-
-	double trainImagesPixels (int,int);
-
-	double testImagesPixels (int,int);
-
-	std::vector <double> trainImages (int);
-
-	std::vector <double> testImages (int);
+	Eigen::VectorXd testImage (int);
 	
 
 
 
 private : 
 	//Path to access the MNIST data base in the computer
-	std::string path;
+	std::string _path;
 
 	//Vector of 2 containing the names of the files as strings
 	//See constructor
-	std::vector<std::string> images_file_names ;
+	std::vector<std::string> _images_file_names ;
 
-	std::vector<std::string> labels_file_names ;
+	std::vector<std::string> _labels_file_names ;
 
 	//Class attributes containing the images
-	std::vector <std::vector <double> > xtrain ;
+	std::vector < Eigen::VectorXd > _xtrain ;
 
-	std::vector <std::vector <double> > xtest ;
+	std::vector <Eigen::VectorXd> _xtest ;
 
 	//Class attributes containing the labels
-	std::vector <int> ytrain ;
+	std::vector <int> _ytrain ;
 
-	std::vector <int> ytest ;
-
-	//All the integer in the MNIST data base are stored in the high endian format
-	//Thus we need a process to transform high endian stored integers into low endian integers
-	//We did not thought of the transformation algorithm ourselves (we got a lot of help from the internet)
-	int highEndianLowEndian (int i);
+	std::vector <int> _ytest ;
 
 };
 
